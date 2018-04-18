6b11190af46abfa136871fe9f8b4a21462a63aa7
shamiul94 <37493667+shamiul94@users.noreply.github.com>
2018-04-18 11:56:22 +0600

    Create Online 1.asm

---
+++ Online 1.asm
@@ -0,0 +1,40 @@
+.MODEL SMALL
+
+.STACK 1000h
+
+.DATA
+    s   db 'a','b','c','d','e',
+                db 'f','g','h','i','j','k',
+                db 'l','m','n','o','p','q',
+                db 'r','s','t',,'u','v','w',
+                db 'x','y','z','$'
+
+
+a dw ?
+.CODE
+main: 
+
+    mov ax, @data
+    mov ds, ax 
+    
+    mov ah , 1 
+    int 21h 
+    
+    mov cl , al 
+    mov ch , 0 
+               
+               
+
+    sub cx ,96
+
+    mov ah , 9
+   
+    lea dx , s     
+    add dx , cx
+    
+    int 21h 
+
+    mov ax, 4Ch
+    int 21h 
+    
+END main
